29bf5d1e5abc4fb4c9a4022dbe880aa5a6487078
Parteim <Sergej_kajnov@mail.ru>
2020-02-10 12:59:11 +0300

    upgrade

--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
 import requests
 from bs4 import BeautifulSoup as BS
+import time
+
+url = 'http://teh-treid.com/'
 
 
 def run():
-    pass
+    start = time.time()
+    response = requests.get(url)
+
+    soup = BS(response.content, 'html.parser')
+
+    links_list = soup.find_all('div', class_='spisok')[1].find_all('a', class_='mobcatl')
+
+    item_url = []
+
+    for link in links_list:
+        if 'Запчасти' in link.text:
+            item_url.append(link)
+
+    print(item_url)
+
+    end = time.time()
+    print(end - start)
 
 
 if __name__ == '__main__':
